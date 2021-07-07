7808465eb8407cc82aa46a10e7e9e0338cd942db
Becky Richardson <rebecca.s.richardson4@gmail.com>
2021-07-06 19:15:12 -0500

    Types Strings

--- playground/types.js
+++ playground/types.js
@@ -1,6 +1,23 @@
 /* eslint-disable */
-const name = 'wes';
+const first = 'wes';
 const middle = "topher";
 const last = `bos`;
 
 const sentence = 'she\'s so cool'
+
+const song = `Ohhh
+ya
+I like 
+pizza!`;
+
+const hello = `Hello my name is ${first}. Nice to meet you. I am ${100 + 1} years old.`;
+
+const html = `
+    <div>
+        <h2>${first}</h2>
+        <p>${hello}</p>
+    </div>
+`;
+
+document.body.innerHTML = html;
+console.log(html);
