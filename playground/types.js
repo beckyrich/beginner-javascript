df31b70d986c5f8c199116baebdba09e5599619b
Becky Richardson <rebecca.s.richardson4@gmail.com>
2021-07-06 19:31:17 -0500

    Types Numbers

--- playground/types.js
+++ playground/types.js
@@ -1,23 +1,32 @@
 /* eslint-disable */
-const first = 'wes';
-const middle = "topher";
-const last = `bos`;
+// const first = 'wes';
+// const middle = "topher";
+// const last = `bos`;
 
-const sentence = 'she\'s so cool'
+// const sentence = 'she\'s so cool'
 
-const song = `Ohhh
-ya
-I like 
-pizza!`;
+// const song = `Ohhh
+// ya
+// I like 
+// pizza!`;
 
-const hello = `Hello my name is ${first}. Nice to meet you. I am ${100 + 1} years old.`;
+// const hello = `Hello my name is ${first}. Nice to meet you. I am ${100 + 1} years old.`;
 
-const html = `
-    <div>
-        <h2>${first}</h2>
-        <p>${hello}</p>
-    </div>
-`;
+// const html = `
+//     <div>
+//         <h2>${first}</h2>
+//         <p>${hello}</p>
+//     </div>
+// `;
 
-document.body.innerHTML = html;
-console.log(html);
+// document.body.innerHTML = html;
+// console.log(html);
+
+const age = 100;
+const first = 'Wes';
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+const dadGets = smarties % kids;
+console.log(`Each kid gets ${eachKidGets} smarties.`);
