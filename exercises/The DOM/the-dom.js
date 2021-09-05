f6a09944c46d07512ca6c21b9448539c473978fc
Becky Richardson <rebecca.s.richardson4@gmail.com>
2021-09-05 16:22:15 -0500

    DOM Element Properties and Methods

--- exercises/The DOM/the-dom.js
+++ exercises/The DOM/the-dom.js
@@ -1,3 +1,20 @@
 const p = document.querySelector('p');
 const imgs = document.querySelectorAll('.item img');
-console.log(imgs);
+const heading = document.querySelector('h2');
+
+console.log(heading.textContent);
+console.log(heading.innerText);
+// set the textContent property on that element
+// heading.textContent = 'Becky is cool';
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent);
+
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+pizzaList.insertAdjacentText('afterbegin', '🍕');
+pizzaList.insertAdjacentText('beforeend', '🍕');
