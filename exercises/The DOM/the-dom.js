a4e6ee15b13ba5c616e6f7c2bfa087d207ac570c
Becky Richardson <rebecca.s.richardson4@gmail.com>
2021-09-05 19:43:24 -0500

    DOM Classes

--- exercises/The DOM/the-dom.js
+++ exercises/The DOM/the-dom.js
@@ -1,20 +1,32 @@
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const heading = document.querySelector('h2');
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const heading = document.querySelector('h2');
 
-console.log(heading.textContent);
-console.log(heading.innerText);
-// set the textContent property on that element
-// heading.textContent = 'Becky is cool';
 // console.log(heading.textContent);
 // console.log(heading.innerText);
+// // set the textContent property on that element
+// // heading.textContent = 'Becky is cool';
+// // console.log(heading.textContent);
+// // console.log(heading.innerText);
 
-console.log(heading.innerHTML);
-console.log(heading.outerHTML);
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
 
-// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
-pizzaList.insertAdjacentText('afterbegin', '🍕');
-pizzaList.insertAdjacentText('beforeend', '🍕');
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+// pizzaList.insertAdjacentText('afterbegin', '🍕');
+// pizzaList.insertAdjacentText('beforeend', '🍕');
+
+// Classes!!!
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+console.log(pic.classList);
+
+function toggleRound() {
+    pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
