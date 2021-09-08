178c4e45ac1ebee36e324f5782c0ea3eea5e044e
Becky Richardson <rebecca.s.richardson4@gmail.com>
2021-09-08 13:27:18 -0500

    Event Targets

--- exercises/Events/events.js
+++ exercises/Events/events.js
@@ -20,13 +20,28 @@ coolButton.addEventListener('click', hooray);
 // Listen on multiple items
 const buyButtons = document.querySelectorAll('button.buy');
 
-function buyItem() {
-    console.log('Buying Item');
-};
+function handleBuyButtonClick(e) {
+    const button = e.target;
+    // console.log(button.textContent);
+    // console.log(parseFloat(e.target.dataset.price));
+    console.log(e.currentTarget);
+    console.log(e.target === e.currentTarget); //best practice to use currentTarget
+    // Stop this event from bubbling up
+    e.stopPropagation();
+}
 
-function attachListener(buyButton) {
-    console.log('Binding the buy button');
-    buyButton.addEventListener('click', buyItem);
-};
+buyButtons.forEach(function(buyButton) {
+    buyButton.addEventListener('click', handleBuyButtonClick);
+});
 
-buyButtons.forEach(attachListener);
+window.addEventListener('click', function(e) {
+    console.log('You clicked on the window');
+    console.log(e.target);
+});
+
+const photoEl = document.querySelector('.photo');
+
+photoEl.addEventListener('mouseenter', function(e) {
+    console.count(e.currentTarget);
+    console.log(this);  //in EventListeners, best practice to not use 'this'. Use e.currentTarget or e.target
+});
