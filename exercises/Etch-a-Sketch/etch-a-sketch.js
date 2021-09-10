f0a11d0139c1c501512caa25a049c09e3d917f2b
Becky Richardson <rebecca.s.richardson4@gmail.com>
2021-09-10 16:01:58 -0500

    Etch-A-Sketch Part 2

--- exercises/Etch-a-Sketch/etch-a-sketch.js
+++ exercises/Etch-a-Sketch/etch-a-sketch.js
@@ -2,7 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
-const MOVE_AMOUNT = 10;
+const MOVE_AMOUNT = 20;
 
 // Setup our canvas for drawing
 //make a variable called height and width from the same properties on our canvas.
@@ -16,6 +16,9 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = MOVE_AMOUNT;
 
+let hue = 0;
+
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.beginPath(); // start the drawing
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
@@ -23,12 +26,29 @@ ctx.stroke();
 
 // Write a draw function
 function draw({ key }) {
+    // increment the hue
+    hue += 10;
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     console.log(key);
     ctx.beginPath();
     ctx.moveTo(x, y);
     // move our x and y values depending on what the user did
-    x -= MOVE_AMOUNT;
-    y -= MOVE_AMOUNT;
+    switch (key) {
+        case 'ArrowUp': 
+            y -= MOVE_AMOUNT;
+            break;
+        case 'ArrowDown':
+            y += MOVE_AMOUNT;
+            break;
+        case 'ArrowLeft':
+            x -= MOVE_AMOUNT;
+            break;
+        case 'ArrowRight':
+            x += MOVE_AMOUNT;
+            break;
+        default:
+            break;
+    }
     ctx.lineTo(x,y);
     ctx.stroke()
 }
@@ -42,6 +62,17 @@ function handleKey(e) {
 }
 
 // Clear/shake function
+function clearCanvas() {
+    canvas.classList.add('shake');
+    ctx.clearRect(0, 0, width, height);
+    canvas.addEventListener('animationend', function() {
+        console.log('Done the shake!');
+        canvas.classList.remove('shake');
+    }, 
+    { once: true }
+    );
+}
 
 // Listen for arrow keys
 window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);
